5e031d6608883a188d2b642ab3edaf7971cb5274
EliseLei <eliseleininger@gmail.com>
2025-04-21 11:18:43 +0200

    replace some styles by tailwind classes

--- public/script/menu.js
+++ public/script/menu.js
@@ -2,18 +2,18 @@ function menu() {
     $('.btn-burger').click(function(){
         if($('.main-menu').css('left') == '0px'){
             $(this).removeClass('close').addClass('open');
-            $('.main-menu, .menu-mobile-fond').animate({'left':'-100%'}, {duration:'fast'});
+            $('.main-menu, #background-mobile').animate({'left':'-100%'}, {duration:'fast'});
 
         } else {
             $(this).removeClass('open').addClass('close');
-            $('.main-menu, .menu-mobile-fond').animate({'left':0}, {duration:'fast'});
+            $('.main-menu, #background-mobile').animate({'left':0}, {duration:'fast'});
         }
      });
 }
 
 function scrollTo() {
     $('.js-scrollTo').on('click', function() {
-        $('.main-menu, .menu-mobile-fond').animate({'left':'-100%'}, {duration:'fast'});
+        $('.main-menu, #background-mobile').animate({'left':'-100%'}, {duration:'fast'});
         var page = $(this).attr('href');
         var speed = 750; 
         $('html, body').animate( { scrollTop: $(page).offset().top -70 }, speed ); 
